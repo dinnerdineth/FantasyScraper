1074dc2055ff1cac6108319566151fa0f15254f5
dinnerdineth <dinethd94@yahoo.com.au>
2023-11-12 15:53:35 +1100

    Added scrapbook file Batter and Pitcher name scraping is now working

--- RosterScraper.py
+++ RosterScraper.py
@@ -4,12 +4,35 @@ import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 
 #link to each day
 #scoring period goes from 1 - 186
 #https://fantasy.espn.com/baseball/team?leagueId=209255785&seasonId=2023&teamId=10&scoringPeriodId=1&statSplit=singleScoringPeriod
+
+def batter_scraper(driver):
+  batters = []
+  for i in range(1,15):
+    try:
+      player_name_object = driver.find_element(By.XPATH,'//*[@id="fitt-analytics"]/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[1]/div/div/table[1]/tbody/tr[%s]/td[2]/div/div/div[2]/div/div[1]'%i)
+      player_name = player_name_object.get_attribute('title')
+    except NoSuchElementException:
+      player_name = "NaN"
+    batters.append(player_name)
+  return batters
+
+def pitcher_scraper(driver):
+  pitchers = []
+  for i in range(1,15):
+    try:
+      player_name_object = driver.find_element(By.XPATH,'//*[@id="fitt-analytics"]/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[2]/div/div/table[1]/tbody/tr[%s]/td[2]/div/div/div[2]/div/div[1]'%i)
+      player_name = player_name_object.get_attribute('title')
+    except NoSuchElementException:
+      player_name = "NaN"
+    pitchers.append(player_name)
+  return pitchers
+      
 
 def roster_scraper():
   x = 1
@@ -25,24 +48,8 @@ def roster_scraper():
   wait = WebDriverWait(driver, 20)
   wait.until(lambda d : trade_limit_label.is_displayed())
   
-  #create blank list with player names
-  players = []
-  
-  #Get player name
-  wait = WebDriverWait(driver, 10)
-  
-  #players = [title.text for title in wait.until(EC.visibility_of_all_elements_located((By.CLASS_NAME, 'jsx-1811044066 player-column__athlete flex')))]
-  elements = driver.find_elements(By.CLASS_NAME,"jsx-1811044066 player-column__athlete flex")
-  #elements = driver.find_elements("class name","jsx-1811044066 player-column__athlete flex")
-  print(elements)
-  # for element in elements:
-  #   player = element.get_attribute('title')
-  #   print(player)
-  #   players.append(player)
-  
-  print(players)
-  #div class for player names
-  #jsx-1811044066 player-column__athlete flex
-  #get title
+  print(batter_scraper(driver))
+  print(pitcher_scraper(driver))
 
-  
+
+  driver.quit()
---
+++ scraper_scrapbook.py
@@ -0,0 +1,48 @@
+#Testing using different scraping methods
+
+import pandas 
+import csv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+#library for no such element exception
+from selenium.common.exceptions import NoSuchElementException
+
+
+#Load website..this is constant
+x = 1
+url = ("https://fantasy.espn.com/baseball/team?leagueId=209255785&seasonId=2023&teamId=10&scoringPeriodId=%s&statSplit=singleScoringPeriod"% x)
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get(url)
+driver.implicitly_wait(30)
+#identify object to detect page is loaded
+#trade_limit_label = driver.find_element(By.NAME,'Trade & Acquisition Limits')
+trade_limit_label =  driver.find_element("xpath",'//*[@id="fitt-analytics"]/div/div[5]/div[2]/div[3]/div/div/div/div[1]/div[2]/div/div/span')
+#print(trade_limit_label)
+wait = WebDriverWait(driver, 20)
+wait.until(lambda d : trade_limit_label.is_displayed())
+
+#Scrape 1 player only - position player
+adley_object = driver.find_element(By.XPATH,'//*[@id="fitt-analytics"]/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[1]/div/div/table[1]/tbody/tr[1]/td[2]/div/div/div[2]/div/div[1]') #picked up correct object
+#adley_object = driver.find_element(By.CLASS_NAME,'jsx-1811044066 player-column__athlete flex')
+adley_name = adley_object.get_attribute('title')
+#print(adley_object)
+print(adley_name)
+
+#Scrape 1 player only - pitcher
+sandy_object = driver.find_element(By.XPATH,'//*[@id="fitt-analytics"]/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[2]/div/div/table[1]/tbody/tr[1]/td[2]/div/div/div[2]/div/div[1]')
+sandy_name = sandy_object.get_attribute('title')
+print(sandy_name)
+
+#Attempt to scrape if row has no player name
+try:
+  blank_object = driver.find_element(By.XPATH,'//*[@id="fitt-analytics"]/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[2]/div/div/table[1]/tbody/tr[8]/td[2]/div/div/div[2]/div/div[1]') #Jhoan Druan + 1 row
+  blank_name = blank_object.get_attribute('title')
+except NoSuchElementException:
+  blank_name = "NaN"  
+print(blank_name)
+
+driver.quit()
+
