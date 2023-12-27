69f07db733c23c053c304ca9362c0be55de19ec5
dinnerdineth <dinethd94@yahoo.com.au>
2023-12-27 12:10:16 +1100

    Deleted branch and reverted to main. Removed implicit wait to improve performance

--- RosterScraper.py
+++ RosterScraper.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
@@ -32,12 +33,17 @@ def batter_scraper(driver,batters):
 def pitcher_scraper(driver,pitchers):
   #pitchers = []
   for i in range(1,15):
+    start_time = time.perf_counter()
     try:
       player_name_object = driver.find_element(By.XPATH,'//*[@id="fitt-analytics"]/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[2]/div/div/table[1]/tbody/tr[%s]/td[2]/div/div/div[2]/div/div[1]' % i)
       player_name = player_name_object.get_attribute('title')
     except NoSuchElementException:
       player_name = "NaN"
     pitchers.append(player_name)
+    
+    end_time = time.perf_counter()
+    for_loop_runtime = end_time - start_time
+    print("%s took %s to scrape" % (player_name,for_loop_runtime))
   return pitchers
       
 
@@ -52,12 +58,13 @@ def roster_scraper():
     driver = webdriver.Chrome()
     driver.maximize_window()
     driver.get(url)
-    driver.implicitly_wait(30)
+    #driver.implicitly_wait(5)
     
     #identify object to detect page is loaded    
-    trade_limit_label =  driver.find_element("xpath",'//*[@id="fitt-analytics"]/div/div[5]/div[2]/div[3]/div/div/div/div[1]/div[2]/div/div/span')
     wait = WebDriverWait(driver, 20)
-    wait.until(lambda d : trade_limit_label.is_displayed())
+    wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="fitt-analytics"]/div/div[5]/div[2]/div[3]/div/div/div/div[1]/div[2]/div[1]/div/span')))
+    #trade_limit_label =  driver.find_element("xpath",'//*[@id="fitt-analytics"]/div/div[5]/div[2]/div[3]/div/div/div/div[1]/div[2]/div/div/span')
+    #wait.until(lambda d : trade_limit_label.is_displayed())
 
     #Perform scrape  
     batter_scraper(driver,batters)
