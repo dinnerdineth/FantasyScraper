9193da1f264cee775d0f8a9171868fe86ce251ff
dinnerdineth <dinethd94@yahoo.com.au>
2024-01-01 12:12:30 +1100

    Scraped individual batter stats and points. Swapped batters array to DataFrame. Need to append each batters stats to the df

--- RosterScraper.py
+++ RosterScraper.py
@@ -1,6 +1,5 @@
 #scrape daily roster and points
-import pandas 
-import csv
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -23,14 +22,49 @@ def batter_scraper(driver,batters):
     is_totals_row_object = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[1]/div/div/table[1]/tbody/tr[%s]/td[3]/div' % row_num)
     if is_totals_row_object.get_attribute('innerText') == "TOTALS":
       totals_row = True
+      break
     else:
+      batter_dict = {'Batter' : '','Runs' : 0,'Total Bases' : 0,'Walks' : 0,'Strike Outs' : 0,'Stolen Bases' : 0,'Points' : 0}
       batter_name_obj = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[1]/div/div/table[1]/tbody/tr[%s]/td[2]/div/div/div[2]/div/div[1]' %  row_num)
       try:
         batter_name = batter_name_obj.get_attribute('title')
       except NoSuchElementException:
         batter_name = "NaN"
-    print(batter_name)
-    batters.append(batter_name)
+        
+      if batter_name != "NaN": #if valid player get other stats
+        batter_dict['Batter'] = batter_name
+        
+        runs_obj = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[1]/div/div/div/div[2]/table/tbody/tr[%s]/td[1]/div' % row_num)
+        runs = runs_obj.get_attribute('innerText')
+        batter_dict['Runs'] = runs
+        
+        total_base_obj = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[1]/div/div/div/div[2]/table/tbody/tr[%s]/td[2]/div' % row_num)
+        total_bases = total_base_obj.get_attribute('innerText')
+        batter_dict['Total Bases'] = total_bases
+        
+        walks_obj = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[1]/div/div/div/div[2]/table/tbody/tr[%s]/td[3]/div' % row_num)
+        walks = walks_obj.get_attribute('innerText')
+        batter_dict['Walks'] = walks
+        
+        ks_obj = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[1]/div/div/div/div[2]/table/tbody/tr[%s]/td[4]/div' % row_num)
+        ks = ks_obj.get_attribute('innerText')
+        batter_dict['Strike Outs'] = ks
+
+        sb_obj = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[1]/div/div/div/div[2]/table/tbody/tr[%s]/td[5]/div' % row_num)
+        sb = sb_obj.get_attribute('innerText')
+        batter_dict['Stolen Bases'] = sb
+        
+        points_obj = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[1]/div/div/table[2]/tbody/tr[%s]/td/div' % row_num)
+        points = points_obj.get_attribute('title')
+        points = points.strip(" points")
+        batter_dict['Points'] = points
+     
+    #Need to get dictionary added to dataframe
+    
+    #batter_dict_df = pd.DataFrame(data=batter_dict)
+    #print(batter_dict_df)
+    #batters = pd.concat([batters,batter_dict_df],ignore_index=True)
+    #batters.append(batter_name, ignore_index=True)
     row_num += 1
     
   # end_time = time.perf_counter()
@@ -71,7 +105,8 @@ def roster_scraper():
   start_time = time.perf_counter()
   x = 1
   pitchers = []
-  batters = []
+  #batters = []
+  batters = pd.DataFrame(columns = ['Batter','Runs','Total Bases','Walks','Strike Outs','Stolen Bases','Points'])
   #Create for loop to scrape each day
   for x in range(1,2):
     
