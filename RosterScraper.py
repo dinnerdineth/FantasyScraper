f316461bb1239452e095d70c306bf927a5c4144c
dinnerdineth <dinethd94@yahoo.com.au>
2024-01-02 12:30:29 +1100

    Finished pitcher scrape to dataframe script. DFs in roster_scraper script are empty

--- RosterScraper.py
+++ RosterScraper.py
@@ -1,4 +1,6 @@
 #scrape daily roster and points
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning) #suppress the warning about append getting removed in the future
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -59,13 +61,14 @@ def batter_scraper(driver,batters):
         points = points.strip(" points")
         batter_dict['Points'] = points
         
-        print(batters)
+        #print(batters)
         #print(batter_dict)
      
         
     batters = batters.append(batter_dict, ignore_index=True)
     row_num += 1
-    
+  print(batters)
+  return batters
   # end_time = time.perf_counter()
   # function_run_time = end_time - start_time
   # print("Batter Function took %s seconds to run" % function_run_time)
@@ -82,29 +85,74 @@ def pitcher_scraper(driver,pitchers):
     #print(is_totals_row_object.get_attribute('innerText'))
     if is_totals_row_object.get_attribute('innerText') == "TOTALS":
       totals_row = True
+      break
     else: #get player name and daily points total
+      pitcher_dict = {'Pitcher' : '', 'IP' : 0.0, 'Hits' : 0, 'ER': 0, 'Walks' : 0, 'Strike Outs' : 0, 'W' : 0, 'L' : 0, 'Saves' : 0, 'Holds': 0, 'Points': 0}
       pitcher_name_obj = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[2]/div/div/table[1]/tbody/tr[%s]/td[2]/div/div/div[2]/div/div[1]' % row_num)
       try:
         pitcher_name = pitcher_name_obj.get_attribute('title')
       except NoSuchElementException:
         pitcher_name = "NaN"
+    if pitcher_name != "NaN":
+      pitcher_dict['Pitcher'] = pitcher_name
       
-      print(pitcher_name)  
-      pitchers.append(pitcher_name)
+      ip_obj = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[2]/div/div/div/div[2]/table/tbody/tr[%s]/td[1]/div' % row_num)
+      ip = ip_obj.get_attribute('innerText')
+      pitcher_dict['IP'] = ip
+      
+      hits_obj = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[2]/div/div/div/div[2]/table/tbody/tr[%s]/td[2]/div' % row_num)
+      hits = hits_obj.get_attribute('innerText')
+      pitcher_dict['Hits'] = hits
+      
+      er_obj = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[2]/div/div/div/div[2]/table/tbody/tr[%s]/td[3]/div' % row_num)
+      er = er_obj.get_attribute('innerText')
+      pitcher_dict['ER'] = er
+      
+      walks_obj = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[2]/div/div/div/div[2]/table/tbody/tr[%s]/td[4]/div' % row_num)
+      walks = walks_obj.get_attribute('innerText')
+      pitcher_dict['Walks'] = walks
+      
+      ks_obj = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[2]/div/div/div/div[2]/table/tbody/tr[%s]/td[5]/div' % row_num)
+      ks = ks_obj.get_attribute('innerText')
+      pitcher_dict['Strike Outs'] = ks
+      
+      wins_obj = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[2]/div/div/div/div[2]/table/tbody/tr[%s]/td[6]/div' % row_num)
+      wins = wins_obj.get_attribute('innerText')
+      pitcher_dict['W'] = wins
+      
+      loss_obj = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[2]/div/div/div/div[2]/table/tbody/tr[%s]/td[7]/div' % row_num)
+      loss = loss_obj.get_attribute('innerText')
+      pitcher_dict['L'] = loss
+      
+      saves_obj = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[2]/div/div/div/div[2]/table/tbody/tr[%s]/td[8]/div' % row_num)
+      saves = saves_obj.get_attribute('innerText')
+      pitcher_dict['Saves'] = saves
+      
+      hold_obj = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[2]/div/div/div/div[2]/table/tbody/tr[%s]/td[9]/div' % row_num)
+      hold = hold_obj.get_attribute('innerText')
+      pitcher_dict['Holds'] = hold
+      
+      points_obj = driver.find_element(By.XPATH,'/html/body/div[1]/div[1]/div/div/div[5]/div[2]/div[3]/div/div/div/div[3]/div/div[2]/div/div/table[2]/tbody/tr[%s]/td/div' % row_num)
+      points = points_obj.get_attribute('title')
+      points = points.strip(" points")
+      pitcher_dict['Points'] = points
+      
+      pitchers = pitchers.append(pitcher_dict,ignore_index=True)
       row_num += 1
       
   # end_time = time.perf_counter()
   # function_run_time = end_time - start_time
   # print("Pitcher Function took %s seconds to run" % function_run_time)
   
+  print(pitchers)
   return pitchers
   
 
 def roster_scraper():
   start_time = time.perf_counter()
   x = 1
-  pitchers = []
-  #batters = []
+  #Create blank dataframes for pitchers and batters
+  pitchers = pd.DataFrame(columns=['Pitcher','IP','Hits','ER','Walks','Strike Outs','W','L','Saves','Holds','Points'])
   batters = pd.DataFrame(columns = ['Batter','Runs','Total Bases','Walks','Strike Outs','Stolen Bases','Points'])
   #Create for loop to scrape each day
   for x in range(1,2):
