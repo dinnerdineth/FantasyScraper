cfa62a98e0eee4f5ac9322a386c447a96ee1d1ad
dinnerdineth <dinethd94@yahoo.com.au>
2024-01-23 20:49:00 +1100

    Created analysis script

---
+++ analysis.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+def player_count(df): #This function counts the number of unique values in a datafram column to count the number of pitchers and hitters used
+  return
+
+
+def main():
+  return
